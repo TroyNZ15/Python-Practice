0abf9099193bf890f2f54e5f59ba1dd5f7b39f09
TroyNZ15 <159851792+TroyNZ15@users.noreply.github.com>
2024-05-02 12:28:05 +1200

    Version -1

---
+++ main.py
@@ -0,0 +1,35 @@
+# Ask the user their name
+# Greet the user and introduce them to the quiz
+# Ask the user a question
+# Tell the user the answer
+# End the quiz
+
+# Introduction
+print("hello there")
+
+# Question
+name= input("what is your name?")
+
+# Statement
+print("welcome to this quiz on what to do if you encounter a bear")
+
+# Statement
+print("if it's brown lay down, if it's black fight back, if it's white good night")
+
+# Question
+polar=input("if you ever encounter a polar bear, what do you do?")
+
+# Statement
+print("if you encounter a polar bear there is nothing you can do.")
+
+# Question
+input("if you see a brown bear what do you do?")
+
+# Question
+input("if you see a black bear what do you do?")
+
+# Statement
+print("With a brown bear, pretending to be dead is your best chance. With a black bear, scaring it off is your best chance.")
+
+# End of quiz
+print("that is the end of the quiz, thank you for filling this out and I hope you've learned something new!")
